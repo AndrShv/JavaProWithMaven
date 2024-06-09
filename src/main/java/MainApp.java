097849164318c1e0a8346e94e9141b8816ccc290
AndrewShv <150950997+AndrShv@users.noreply.github.com>
2024-06-09 11:19:17 +0300

    Delete src/main/java/MainApp.java

--- src/main/java/MainApp.java
+++
@@ -1,9 +0,0 @@
-public class MainApp {
-    public static void main(String[] args) {
-        PetShelterApp app = new PetShelterApp() {};
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            app.run();
-        }));
-        app.run();
-    }
-}
