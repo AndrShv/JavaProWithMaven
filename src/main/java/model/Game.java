091621fa1d09db0ccc95aefbbc4a0c27db4e1ba0
BobikGavvv <andrey678a@gmail.com>
2024-06-19 18:38:33 +0300

    .

--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -1,4 +1,31 @@
 package model;
 
+import lombok.*;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Game {
+    private int id;
+    private String name;
+    private Date releaseDate;
+    private float rating;
+    private double cost;
+    private String description;
+    private Timestamp creationDate;
+
+    public Game(int id, String gameName, java.util.Date releaseDate, float rating, double cost, String description) {
+        this.id = id;
+        this.name = gameName;
+        this.releaseDate = new java.sql.Date(releaseDate.getTime());
+        this.rating = rating;
+        this.cost = cost;
+        this.description = description;
+        this.creationDate = new Timestamp(System.currentTimeMillis());
+    }
 }
---
+++ src/main/java/model/GameStoreFunctions.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.List;
+
+public interface GameStoreFunctions {
+    void addGame(Game game);
+    void deleteGame(int id);
+    Game searchGameByName(String name);
+    List<Game> filterGamesByCost(double minCost, double maxCost);
+    List<Game> filterGamesByRating(float minRating);
+    List<Game> getAllGamesSortedByCreationDate();
+    List<Game> getAllGames();
+}
